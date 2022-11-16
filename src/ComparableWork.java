46c0b3bbb062de3804091296466e54fe11c1a1ec
recepergan <68998200+recepergan@users.noreply.github.com>
2022-11-16 16:36:45 +0300

    comparable

---
+++ src/ComparableWork.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ComparableWork {
+
+        public static void main(String[] args) {
+
+        ArrayList<Student> al = new ArrayList<Student>();
+        al.add(new Student(101,"Vijay",23));
+        al.add(new Student(106,"Ajay",27));
+        al.add(new Student(105,"Jai",21));
+
+        Collections.sort(al);
+        for(Student st:al) {
+            System.out.println(st.rollno+" "+st.name+" "+st.age);
+        }
+
+    }
+}
+class Student implements Comparable<Student> {
+
+    int rollno;
+    String name;
+    int age;
+
+    public Student(int rollno, String name, int age) {
+        this.rollno = rollno;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Student st) {
+        if (age == st.age)
+            return 0;
+        else if (age > st.age)
+           // return 1;
+            return -1;
+        else
+           // return -1;
+        return 1;
+    }
+}
+
