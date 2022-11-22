21cc751dd170c1c5ca9a9dba51dce63b03675223
recepergan <68998200+recepergan@users.noreply.github.com>
2022-11-22 12:21:37 +0300

    synchronized

---
+++ src/synchronizedd/TestSynchronization3.java
@@ -0,0 +1,34 @@
+package synchronizedd;
+//Program of synchronized method by using annonymous class
+ //Example of synchronized method by using annonymous class
+class Table3{
+    synchronized void printTable(int n){//synchronized method
+        for(int i=1;i<=5;i++){
+            System.out.println(n*i);
+            try{
+                Thread.sleep(400);
+            }catch(Exception e){System.out.println(e);}
+        }
+
+    }
+}
+
+public class TestSynchronization3 {
+    public static void main(String args[]){
+        final Table3 obj = new Table3();//only one object
+
+        Thread t1=new Thread(){
+            public void run(){
+                obj.printTable(5);
+            }
+        };
+        Thread t2=new Thread(){
+            public void run(){
+                obj.printTable(100);
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
+}
